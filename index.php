7f859415745c60dcc768e500aa64feea2b18a401
INCOGNIC <franckaaj@gmail.com>
2024-10-02 22:12:21 -0500

    Mi primer commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="resources/css/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="resources/css/main.css">
+</head>
+
+<body class="px-3">
+    <!-- encabezado -->
+    <div class="row mt-5">
+        <div class="col-sm-12 col-md-6">
+            <h1 class="text-center">EN LINEA</h1>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <p class="text-right pr-5">SOLUCIONES MUNDIALES</p>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-sm-12">
+            <img class="w-100" style="height:350px" src="resources/img/baner01.PNG" alt="">
+        </div>
+    </div>
+    <!-- fin de encabezado -->
+    <div class="container mt-4">
+        <h1 class="h1">SOLUCIONES CREATIVAS</h1>
+        <p>Sin importar cual sea tu negocio, nuestro equipo puede hacer todo desde cero.</p>
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img class="w-100" src="resources/img/idea_01.PNG" alt="">
+                        <h5 class="h5 mt-2"> <i class="bi bi-pen"></i> DISEÑO</h5>
+                        <p>Creamos tus diseños segun tus nesecidades.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img class="w-100" src="resources/img/idea_02.PNG" alt="">
+                        <h5 class="h5 mt-2"> <i class="bi bi-flower1"></i> DESARROLLO</h5>
+                        <p>Nuestros programadores te garantizan la perferccion.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <img class="w-100" src="resources/img/idea_03.PNG" alt="">
+                        <h5 class="h5 mt-2"> <i class="bi bi-dpad-fill"></i> EDICION DE LIBROS</h5>
+                        <p>Que el producto final llegfue a tus clientes.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="bg-wine">
+         <p class="p-5 text-white text-center">Edificio IDEX PJ, Jr. Amazonas 120 - Chachapoyas Chachapoyas - Amazonas.</p> 
+    </footer>
+</body>
+
+</html>
